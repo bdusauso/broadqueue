c2b9498042cc4de14b117a15f8004164a9dcbb69
Bruno Dusausoy <bruno.dusausoy@gmail.com>
2019-04-10 19:00:42 +0200

    Add UUID field to events

--- lib/broadqueue/event.ex
+++ lib/broadqueue/event.ex
@@ -2,6 +2,7 @@ defmodule Broadqueue.Event do
   use Ecto.Schema
 
   schema "events" do
+    field :uuid, Ecto.UUID
     field :payload, :map
   end
 end
--- priv/repo/migrations/20190410143319_create_events.exs
+++ priv/repo/migrations/20190410143319_create_events.exs
@@ -3,7 +3,10 @@ defmodule Broadqueue.Repo.Migrations.CreateEvents do
 
   def change do
     create table("events") do
+      add :uuid, :uuid
       add :payload, :map
     end
+
+    create index("events", [:uuid], unique: true)
   end
 end
