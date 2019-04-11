1e9f553d9d301f73a3c556eba8e7456c990d74ea
Bruno Dusausoy <bruno.dusausoy@gmail.com>
2019-04-11 04:31:49 +0200

    Print error message if no message count is given

--- lib/mix/tasks/publish_messages.ex
+++ lib/mix/tasks/publish_messages.ex
@@ -2,14 +2,21 @@ defmodule Mix.Tasks.PublishMessages do
   use Mix.Task
 
   @shortdoc "Publish 100.000 messages to RabbitMQ"
-  def run(_args) do
-    {:ok, conn} = AMQP.Connection.open
-    {:ok, chan} = AMQP.Channel.open(conn)
-    AMQP.Queue.declare chan, "broadway"
-    AMQP.Exchange.declare chan, "default"
-    AMQP.Queue.bind chan, "broadway", "default"
+  def run(args) do
+    case messages_count(args) do
+      {:ok, count} ->
+        {:ok, conn} = AMQP.Connection.open
+        {:ok, chan} = AMQP.Channel.open(conn)
+        AMQP.Queue.declare(chan, "broadway")
+        AMQP.Exchange.declare(chan, "default")
+        AMQP.Queue.bind(chan, "broadway", "default")
+        AMQP.Confirm.select(chan)
+        publish_messages(chan, count)
+        AMQP.Confirm.wait_for_confirms(chan)
 
-    publish_messages(chan, 100_000)
+      :error ->
+        Mix.Shell.IO.error("Usage: mix publish_messages --count <messages_count>")
+    end
   end
 
   defp publish_messages(chan, count) do
@@ -19,5 +26,15 @@ defmodule Mix.Tasks.PublishMessages do
 
   defp generate_payload(count) do
     Jason.encode!(%{uuid: Ecto.UUID.generate(), payload: %{number: count}})
+  end
+
+  defp messages_count(cli_args) do
+    case OptionParser.parse!(cli_args, strict: [count: :integer]) do
+      {[count: count], _} ->
+        {:ok, count}
+
+      _ ->
+        :error
+    end
   end
 end
