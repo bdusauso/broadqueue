1bbb05ad5ec88fcddb92eb5a18749062d98ef4bd
Bruno Dusausoy <bruno.dusausoy@gmail.com>
2019-04-22 15:33:34 +0200

    Improve code with configuration struct. Add 'duplicate ratio' as configuration switch

--- lib/mix/tasks/publish_messages.ex
+++ lib/mix/tasks/publish_messages.ex
@@ -3,52 +3,104 @@ defmodule Mix.Tasks.PublishMessages do
 
   @chunk_size 10_000
 
+  defmodule Configuration do
+    alias __MODULE__
+
+    defstruct count: 0,
+              fault_ratio: 0,
+              duplicate_ratio: 0
+
+    def new(count, fault_ratio, duplicate_ratio) do
+      config = %Configuration{
+        count: count,
+        fault_ratio: fault_ratio,
+        duplicate_ratio: duplicate_ratio
+      }  
+
+      if valid?(config), do: config, else: raise OptionParser.ParseError
+    end
+
+    def valid?(%Configuration{} = config) do
+      config.count > 0 && 
+      config.fault_ratio >= 0 && 
+      config.fault_ratio <= 100 &&
+      config.duplicate_ratio >= 0 &&
+      config.duplicate_ratio <= 100 &&
+      config.fault_ratio + config.duplicate_ratio <= 100
+    end
+  end
+
+  alias Configuration
+
   @shortdoc "Publish messages to RabbitMQ"
   def run(args) do
     case parse_cli_args(args) do
-      {:ok, {count, fault_ratio}} ->
+      {:ok, config} ->
         {:ok, conn} = AMQP.Connection.open
         {:ok, chan} = AMQP.Channel.open(conn)
         AMQP.Queue.declare(chan, "broadway")
         AMQP.Exchange.declare(chan, "default")
         AMQP.Queue.bind(chan, "broadway", "default")
         AMQP.Confirm.select(chan)
-        publish_messages(chan, count, fault_ratio)
+        publish_messages(chan, config)
 
       :error ->
-        Mix.Shell.IO.error("Usage: mix publish_messages --count <messages_count> --fault-ratio <percentage>")
+        Mix.Shell.IO.error("""
+          Usage: mix publish_messages --count <messages_count> --fault-ratio <percentage> --duplicate-ratio <percentage>
+        """)
     end
   end
 
-  defp publish_messages(chan, count, fault_ratio) do
-    (1..count)
+  defp publish_messages(chan, config) do
+    config
+    |> generate_payloads()
     |> Stream.chunk_every(@chunk_size)
-    |> Enum.each(&(publish_chunk(&1, chan, fault_ratio)))
+    |> Enum.each(&(publish_chunk(&1, chan, config)))
   end
 
-  defp publish_chunk(chunk, chan, fault_ratio) do
-    Enum.each(chunk, &(AMQP.Basic.publish chan, "default", "", generate_payload(&1, fault_ratio)))
+  defp publish_chunk(chunk, chan, config) do
+    Enum.each(chunk, &(AMQP.Basic.publish chan, "default", "", &1))
     AMQP.Confirm.wait_for_confirms(chan)
   end
 
-  defp generate_payload(count, fault_ratio) do
-    if :rand.uniform(100) >= fault_ratio do
-      Jason.encode!(%{uuid: Ecto.UUID.generate(), payload: %{number: count}})
-    else
-      "faulty_string"
-    end
+  defp generate_payloads(config) do
+    duplicates = List.duplicate(generate_payload(config), (div(config.count, 100)) * config.duplicate_ratio) 
+    faults = List.duplicate("faulty_string", (div(config.count, 100)) * config.fault_ratio)
+    valids = for _ <- 1..(config.count - length(duplicates) - length(faults)), do: generate_payload(config)
+
+    payloads = 
+      (duplicates ++ faults ++ valids)
+      |> Enum.shuffle()
+      |> Stream.with_index()
+      |> Stream.map(fn 
+        {"faulty_string", _} -> "faulty_string"
+        {map, i} -> map |> Map.put_new(:payload, %{number: i}) |> Jason.encode!
+      end)
+      |> Enum.to_list()
+  end
+
+  defp generate_payload(config) do
+    %{uuid: Ecto.UUID.generate()}
   end
 
   defp parse_cli_args(cli_args) do
     try do
+      switches = [
+        count: :integer, 
+        fault_ratio: :integer, 
+        duplicate_ratio: :integer
+      ]
       {parsed_args, _}
-        = OptionParser.parse!(cli_args, switches: [count: :integer, fault_ratio: :integer]) |> IO.inspect
+        = OptionParser.parse!(cli_args, switches: switches)
 
-      count = Keyword.get(parsed_args, :count, 10)
-      fault_ratio = Keyword.get(parsed_args, :fault_ratio, 0)
-      if count <= 0 || fault_ratio < 0 || fault_ratio > 100, do: raise OptionParser.ParseError
+      config = Configuration.new(
+        Keyword.get(parsed_args, :count, 10),
+        Keyword.get(parsed_args, :fault_ratio, 0),
+        Keyword.get(parsed_args, :duplicate_ratio, 0)  
+      )
 
-      {:ok, {count, fault_ratio}}
+
+      {:ok, config}
     rescue
       _ ->
         :error
