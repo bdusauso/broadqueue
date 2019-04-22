0d64df2b2e7fff5900f0b372e4886ee715e8d132
Bruno Dusausoy <bruno.dusausoy@gmail.com>
2019-04-22 22:08:09 +0200

    Everything done in the processor, no batch used

--- lib/broadqueue/worker.ex
+++ lib/broadqueue/worker.ex
@@ -31,46 +31,20 @@ defmodule Broadqueue.Worker do
         default: [
           stages: 1
         ]
-      ],
-      batchers: [
-        storage: [stages: 1, batch_size: batch_size],
-        dead_letter: []
       ]
     )
   end
 
   @impl true
   def handle_message(_, message, _) do
-    try do
-      message
-      |> Message.update_data(&decode_data/1)
-      |> Message.put_batcher(:storage)
-    rescue
-      _ -> Message.put_batcher(message, :dead_letter)
-    end
+    Event
+    |> struct!(message |> decode_data())
+    |> Repo.insert!()
+
+    message
   end
 
-  @impl true
-  def handle_batch(:storage, messages, _batch_info, _context) do
-    events =
-      messages
-      |> Stream.with_index()
-      |> Flow.from_enumerable(max_demand: 1)
-      |> Flow.map(&process_batch_message/1)
-      |> Enum.sort_by(fn {_, index} -> index end)
-      |> Enum.map(fn {message, _} -> message.data end)
-
-    Repo.insert_all(Event, events)
-
-    messages
-  end
-
-  def handle_batch(:dead_letter, messages, _, _) do
-    Logger.error("Discarding #{length(messages)} messages")
-    messages
-  end
-
-  defp decode_data(data) do
+  defp decode_data(%Message{data: data}) do
     data
     |> Jason.decode!
     |> atomize_keys
@@ -80,9 +54,5 @@ defmodule Broadqueue.Worker do
     map
     |> Enum.map(fn {k, v} -> {String.to_atom(k), v} end)
     |> Enum.into(%{})
-  end
-
-  defp process_batch_message({_message, _} = indexed_message) do
-    indexed_message
   end
 end
